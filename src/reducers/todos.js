f51c7d1a8af12ff763cf75d920a620e1c753cf17
Abdullah Alram <natsugrau557@gmail.com>
2021-06-15 17:07:52 +0300

    added update and completed item

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,21 +1,40 @@
 // import { AsyncStorage } from "react-native";
 
-export const todos = (state=[], action) =>{// it is not just adding item, it is all about the list of itmes.
-    switch(action.type){
-        case"ADD_ITEM": {
-            const updatedState = [...state, {...action.payload}]
-            // AsyncStorage.setItem('todos', JSON.stringify(updatedState))
-            return updatedState;//
+export const todos = (state = [], action) => {
+  // it is not just adding item, it is all about the list of itmes.
+  switch (action.type) {
+    case 'ADD_ITEM': {
+      const updatedState = [...state, {...action.payload}];
+      // AsyncStorage.setItem('todos', JSON.stringify(updatedState))
+      return updatedState; //
+    }
+    case 'DELETE_ITEM': {
+      return state.filter (item => {
+        return item.id !== action.payload.id;
+      });
+    }
+    case 'COMPLETED_ITEM': {
+      return state.map (item => {
+        if (item.id === action.payload.id) {
+          return {...item, completed: action.payload.completed};
+        } else {
+          return item;
         }
-        case "DELETE_ITEM":{
-            return state.filter((item) => {
-                return item.id !== action.payload.id
-            });
-        }
-        default:
-            return state;
+      });
+    }
 
-    }    
-}
+    case 'UPDATE_ITEM':
+      return state.map (item => {
+        if (item.id === action.payload.id) {
+          return action.payload;
+        } else {
+          return item;
+        }
+      });
+
+    default:
+      return state;
+  }
+};
 
 // export default addItem
